5920a4c2da332fc9895a0ef0042b44a723abe54d
ben <energy95272z@gmail.com>
2023-07-20 19:55:39 +0800

    change config setting

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    /*
+    ## 新增 base : 用來設定開發中(dev)、產品路徑(build) 所產生的路徑：
+      - 參考官方中文設定路徑：https://cn.vitejs.dev/config/shared-options.html#mode
+      - production 用於 build
+      - development 用於 dev
+      - 設定是產品路徑，所套用的 /react-vite-sample/ 等同 github專案的名稱
+  */
+    base: process.env === 'production' ? '/react-vite-sample/' : '/',
+    plugins: [react()],
+});
