1df8a4347317e52e6aba2eb4728e554373c7cc6c
ben <energy95272z@gmail.com>
2023-08-14 01:05:36 +0800

    test4

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,25 @@ import useProductContext from './hooks/useProductContext';
 import axios from 'axios';
 
 function App() {
+    const [data, setData] = useState([]);
     useEffect(() => {
         (async () => {
             try {
                 const result = await axios.get('/data.json');
                 console.log(result.data);
+                setData(result.data);
             } catch (error) {
                 console.log(error);
             }
         })();
     }, []);
 
-    return <div>測試中2</div>;
+    return (
+        <div>
+            測試中2
+            {data.length ? <img src={data[0].image} alt='' /> : null}
+        </div>
+    );
 }
 
 export default App;
